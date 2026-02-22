a5ea68697ddb740709586f9f7d35bbf16f235afc
shreyash <shreyashsundechamutha66@gmail.com>
2026-02-22 19:53:05 +0530

    Add project files.

---
+++ Cut-Coffee-Paste/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cut_Coffee_Paste.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
